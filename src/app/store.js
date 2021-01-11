fb2870913e5a77b564115ec13b60817847f1f8f0
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-11 11:04:57 +0100

    Add correctLettersReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import vocabularyReducer from '../features/vocabulary/vocabularySlice';
 import wordsReducer from '../features/wordsSlice';
 import gameStatusReducer from '../features/gameStatusSlice';
-
+import correctLettersReducer from '../features/letters/correctLettersSlice';
 export default configureStore({
     reducer: {
         vocabulary: vocabularyReducer,
         words: wordsReducer,
-        gameStatus: gameStatusReducer
+        gameStatus: gameStatusReducer,
+        correctLetters: correctLettersReducer
     }
 })
