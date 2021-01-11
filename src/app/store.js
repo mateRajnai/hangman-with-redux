6681dff726f72e061b727d2274e631636b011135
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-11 11:22:10 +0100

    Add wrongLettersReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import vocabularyReducer from '../features/vocabulary/vocabularySlice';
 import wordsReducer from '../features/wordsSlice';
 import gameStatusReducer from '../features/gameStatusSlice';
 import correctLettersReducer from '../features/letters/correctLettersSlice';
+import wrongLettersReducer from '../features/letters/wrongLettersSlice';
 export default configureStore({
     reducer: {
         vocabulary: vocabularyReducer,
         words: wordsReducer,
         gameStatus: gameStatusReducer,
-        correctLetters: correctLettersReducer
+        correctLetters: correctLettersReducer,
+        wrongLetters: wrongLettersReducer
     }
 })
