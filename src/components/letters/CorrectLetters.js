63345f2b1f379d936199f189b8b1e34dac9c7c54
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-11 10:25:43 +0100

    Import actions of gameStatusSlice in CorrectLetters.js

--- src/components/letters/CorrectLetters.js
+++ src/components/letters/CorrectLetters.js
@@ -2,13 +2,12 @@ import React, {useContext, useEffect} from 'react';
 import styled from 'styled-components';
 import {v4 as uuidv4} from 'uuid';
 import {LettersContext} from '../../context/LettersContext';
-import {GameStatusContext} from '../../context/GameStatusContext';
 import CorrectLetter from './CorrectLetter';
+import {setIsPlayerWon, setIsEndOfGame} from '../../features/gameStatusSlice';
 
 const CorrectLetters = () => {
 
     const {correctLetters} = useContext(LettersContext);
-    const {setIsEndOfGame, setIsPlayerWon} = useContext(GameStatusContext);
 
     useEffect(() => {
         if (!correctLetters.includes(null) && correctLetters.length !== 0) {
