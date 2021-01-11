22cd8739471ead649b0303856f142e672f800a77
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-11 11:03:51 +0100

    Create correctLettersSlice

---
+++ src/features/letters/correctLettersSlice.js
@@ -0,0 +1,16 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = []; 
+
+const correctLettersSlice = createSlice({
+    name: 'correctLetters',
+    initialState,
+    reducers: {
+
+    }
+
+
+})
+
+export const {} = correctLettersSlice.actions;
+export default correctLettersSlice.reducer;
