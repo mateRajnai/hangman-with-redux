f9a92be13a3d665cb48f166bc695e182a41cba59
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-11 13:32:55 +0100

    Refactor Vocabulary.js using redux state management

--- src/components/Vocabulary.js
+++ src/components/Vocabulary.js
@@ -5,14 +5,33 @@ import englishFlag from '../images/english_flag.png';
 import germanFlag from '../images/german_flag.png';
 import {useDispatch, useSelector} from 'react-redux';
 import {setWordBeforeVisitingVocabularies} from '../features/wordsSlice';
+import {setVocabulary} from '../features/vocabulary/vocabularySlice';
+import {setGeneratedWord} from '../features/wordsSlice';
 
 const Vocabulary = () => {
 
-
-    const {generateWord} = useContext(VocabularyContext);
+    // const {generateWord} = useContext(VocabularyContext);
     const generatedWord = useSelector(state => state.words.generatedWord);
     const vocabulary = useSelector(state => state.vocabulary)
     const dispatch = useDispatch();
+
+    const generateWord = (event) => {
+        const chosenVocabulary = event.target.getAttribute("data-vocabulary") || vocabulary;
+        dispatch(setVocabulary(chosenVocabulary));
+        let generatedWord;
+        switch (chosenVocabulary) {
+            case "English":
+                generatedWord = require('random-words')();
+                break;
+            case "German":
+                generatedWord = require('random-noun-generator-german')();
+                break;
+            default:
+                generatedWord = "apple";
+                break;
+        }
+        dispatch(setGeneratedWord(generatedWord)); 
+    }
 
     useEffect(() => {
         dispatch(setWordBeforeVisitingVocabularies(generatedWord));
