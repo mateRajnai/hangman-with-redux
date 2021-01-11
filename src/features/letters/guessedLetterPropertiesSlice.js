3a8a5f29a2eaa8b1bb537042f689032acec06dd3
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-11 14:27:54 +0100

    Create guessedLetterPropertiesSlice

---
+++ src/features/letters/guessedLetterPropertiesSlice.js
@@ -0,0 +1,20 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    letter: null,
+    isWrong: null
+};
+
+const guessedLetterPropertiesSlice = createSlice({
+    name: 'guessedLetterProperties',
+    initialState,
+    reducers: {
+        setIsGuessedLetterWrong(state, action) {
+            state.isWrong = action.payload;
+        }
+    }
+})
+
+export const {setIsLastlyGuessedLetterWrong} = guessedLetterPropertiesSlice.actions;
+
+export default guessedLetterPropertiesSlice.reducer;
